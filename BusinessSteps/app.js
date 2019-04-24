1edd8a113901464d3beee072225f87826157091b
Dmitry <PDMHub@users.noreply.github.com>
2019-04-24 11:21:22 +0700

    New iteration

--- BusinessSteps/app.js
+++ BusinessSteps/app.js
@@ -6,13 +6,18 @@
 
 function createForceLayout(nodesLinksSchema)
 {
-    var force = d3.layout.force().charge(-1000)
+    var force = d3.layout.force()
+    .charge(-100)
     .size([500,500])
+    .linkDistance([100])
     .nodes(nodesLinksSchema.nodes)
     .links(nodesLinksSchema.links)
-    .on("tick", forceTick);
+    .start();
+    
 
-    d3.select("svg").selectAll("line.link")
+    var svg = d3.select("svg")
+
+    var links = svg.selectAll("line.link")
     .data(nodesLinksSchema.links)
     .enter()
     .append("line")
@@ -21,19 +26,30 @@ function createForceLayout(nodesLinksSchema)
     .style("opacity", 0.8)
     .style("stroke-width", "2");
 
+    var nodes = svg.selectAll("rect")
+    .data(nodesLinksSchema.nodes)
+        .enter()
+        .append("rect")
+        .attr({ x: 10, y: 10, width: 120, height: 60})
+        .style("stroke", "black")
+        .call(force.drag);
 
-    force.start();
+    force.on("tick", forceTick);
 
     function forceTick() {
 
-        d3.selectAll("line.link")
+        links
         .attr("x1", function (d) {return d.source.x;})
         .attr("x2", function (d) {return d.target.x;})
         .attr("y1", function (d) {return d.source.y;})
         .attr("y2", function (d) {return d.target.y;});
-    
-        d3.selectAll("g.node")
-        .attr("transform", function (d) {return "translate("+d.x+","+d.y+")";})
+
+        nodes
+        .attr("x", function (d) {return d.x;})
+        .attr("x", function (d) {return d.x;})
+           
+        //d3.selectAll("g.node")
+        //.attr("transform", function (d) {return "translate("+d.x+","+d.y+")";})
     }
 }
 
