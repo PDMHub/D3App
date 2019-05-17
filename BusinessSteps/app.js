3a6f3e4e406f212a5fc056118e556eb42341ad41
Dmitry <PDMHub@users.noreply.github.com>
2019-05-17 11:19:56 +0700

    Before resting new approach to load json

--- BusinessSteps/app.js
+++ BusinessSteps/app.js
@@ -1,10 +1,12 @@
 ﻿function createForceLayout(nodesLinksSchema)
 {
-    if (error) throw "Error with file loading"
-    var width = 640
-    var height = 480
+    if (error) throw "Error with file loading";
+    var width = 640;
+    var height = 480;
     
-    var simulation = d3.forceSimulation(nodesLinksSchema.nodes)
+    var simulation = d3.forceSimulation(nodesLinksSchema.nodes);
+
+    simulation
     .force('charge', d3.forceManyBody().strength(-20))
     //.force('linkForce', d3.forceLink(nodesLinksSchema.links).distance(20).strength(2))
     .force('center', d3.forceCenter(width/2, height/2))
@@ -13,7 +15,9 @@
         
     function forceTick() {
 
-    var links = d3.select('svg').selectAll('line.link')
+    var links = d3.select('svg').selectAll('line.link');
+
+        links
         .data(nodesLinksSchema.links)
         .enter()
         .append('line')
@@ -26,7 +30,9 @@
         .style('opacity', 0.8)
         .style('stroke-width', '2');
 
-    var nodes = d3.select('svg').selectAll('rect')
+    var nodes = d3.select('svg').selectAll('rect');
+
+        nodes
         .data(nodesLinksSchema.nodes)
         .enter()
         .append('rect')
@@ -36,7 +42,7 @@
         .attr('width', '50')
         .attr('height', '30')
         .style('stroke-width', '2')
-        .style('stroke', 'black')
+        .style('stroke', 'black');
 
 /*         links
         .attr('x1', function (d) {return d.source.x;})
@@ -54,15 +60,15 @@
 }
 
 function initSimulationWithFile(fileToLoad){
-    queue()
+    d3.queue()
     .defer(d3.json, fileToLoad)
-    .await(function(error, jsonSchema) {createForceLayout(jsonSchema)});
-};
+    .await(function(error, jsonSchema) {createForceLayout(jsonSchema);});
+}
 
 function valueDecrease(jsonObject)
 {
    console.log("Все хорошо");
-};
+}
 
 //d3.json('GIISschema.json', function(data) {createForceLayout(data); })
 //createForceLayout(nodesLinks)
