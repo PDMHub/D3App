1f7e83d53389897766ab9b18d445c0d94604a84f
Dmitry <PDMHub@users.noreply.github.com>
2019-05-20 15:36:04 +0700

    First workable version

--- BusinessSteps/app.js
+++ BusinessSteps/app.js
@@ -1,9 +1,17 @@
 ﻿function createForceLayout(nodesLinksSchema)
 {
-    if (error) throw "Error with file loading";
+    //if (error) throw "Error with file loading";
     var width = 640;
     var height = 480;
     
+
+    // Блок кода, который приводит номера источников и целей в связях к 
+    // системе отсчета, начинающейся с нуля
+    for (var link in nodesLinksSchema.links) {
+        nodesLinksSchema.links[link]["source"] = nodesLinksSchema.links[link]["source"] - 1;
+        nodesLinksSchema.links[link]["target"] = nodesLinksSchema.links[link]["target"] - 1;
+      }
+
     var simulation = d3.forceSimulation(nodesLinksSchema.nodes);
 
     simulation
@@ -11,21 +19,22 @@
     //.force('linkForce', d3.forceLink(nodesLinksSchema.links).distance(20).strength(2))
     .force('center', d3.forceCenter(width/2, height/2))
     .force('link', d3.forceLink().links(nodesLinksSchema.links))
+    //.force('link', d3.forceLink(nodesLinksSchema.links))
     .on('tick', forceTick);
         
     function forceTick() {
 
-    var links = d3.select('svg').selectAll('line.link');
+    var links = d3.select('svg').selectAll('line.linkVisual');
 
         links
         .data(nodesLinksSchema.links)
         .enter()
         .append('line')
         .attr('class', 'linkVisual')
-        .attr('x1', d => d.source.x1)
-        .attr('x2', d => d.source.x2)
-        .attr('y1', d => d.source.y1)
-        .attr('y2', d => d.source.y2)
+        .attr('x1', d => d.source.x)
+        .attr('x2', d => d.target.x)
+        .attr('y1', d => d.source.y)
+        .attr('y2', d => d.target.y)
         .style('stroke', 'black')
         .style('opacity', 0.8)
         .style('stroke-width', '2');
@@ -44,35 +53,22 @@
         .style('stroke-width', '2')
         .style('stroke', 'black');
 
-/*         links
-        .attr('x1', function (d) {return d.source.x;})
-        .attr('x2', function (d) {return d.target.x;})
-        .attr('y1', function (d) {return d.source.y;})
-        .attr('y2', function (d) {return d.target.y;});
-
-        nodes
-        .attr('x', function (d) {return d.x;})
-        .attr('y', function (d) {return d.x;}) */
-           
-        //d3.selectAll('g.node')
-        //.attr('transform', function (d) {return 'translate('+d.x+','+d.y+')';})
     }
 }
 
 function initSimulationWithFile(fileToLoad){
-    d3.queue()
-    .defer(d3.json, fileToLoad)
-    .await(function(error, jsonSchema) {createForceLayout(jsonSchema);});
+    
+var loadedSchema = d3.json(fileToLoad)
+                    .then(function (result) {
+                        createForceLayout(result);
+                    }, function (error) {
+                        console.log(error);
+                    });
+
 }
 
-function valueDecrease(jsonObject)
-{
-   console.log("Все хорошо");
-}
 
-//d3.json('GIISschema.json', function(data) {createForceLayout(data); })
-//createForceLayout(nodesLinks)
-initSimulationWithFile("GIISschema.json");
+initSimulationWithFile("http://127.0.0.1:3000/GIISschema.json");
 
 
 
